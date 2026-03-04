4064a02618ca8ab24e0b1773faf37c3c26403634
Niko Zuppas <nzuppas6@gmail.com>
2026-03-04 18:54:48 -0500

    Add mutation_rate parameter to simulate_target function

--- poprl/api.py
+++ poprl/api.py
@@ -9,7 +9,7 @@ from poprl.task import MsprimeTask, SlimTask
 from poprl.envs.msprime_env import MSPRIMEEnv
 from poprl.envs.SLiMEnv import SLiMEnv
 
-def simulate_target(model):
+def simulate_target(model, mutation_rate):
     """Simulate 10 replicates of the target msprime model as a baseline for learning tasks"""
     if isinstance(model, str):
         spec = importlib.util.spec_from_file_location("target_model", model)
@@ -19,25 +19,34 @@ def simulate_target(model):
         
         return ts.allele_frequency_spectrum()
     
-    else:
+    elif isinstance(model, tuple):
         demography, mutation_rate = model
-        samples = {p.name: 10 for p in demography.populations}
-        afs_list = []
-        
-        for _ in range(10):
-            ts = msprime.sim_ancestry(samples=samples, demography=demography, sequence_length=1e6)
-            ts = msprime.sim_mutations(ts, rate=mutation_rate)
-            afs_list.append(ts.allele_frequency_spectrum())
-            
-        return np.mean(afs_list, axis=0)
 
-def make_msprime(model, task=None, tunable=None, randomize_start=False, max_steps=100, observation="sfs"):
+    elif isinstance(model, msprime.Demography):
+        demography = model
+        if mutation_rate is None:
+            mutation_rate = 1e-8
+    
+    else:
+        raise ValueError("model must be an msprime.Demography or a (demography, mutation_rate) tuple")
+    
+    samples = {p.name: 10 for p in demography.populations}
+    afs_list = []
+        
+    for _ in range(10):
+        ts = msprime.sim_ancestry(samples=samples, demography=demography, sequence_length=1e6)
+        ts = msprime.sim_mutations(ts, rate=mutation_rate)
+        afs_list.append(ts.allele_frequency_spectrum())
+            
+    return np.mean(afs_list, axis=0)
+
+def make_msprime(model, mutation_rate=None, task=None, tunable=None, randomize_start=False, max_steps=100, observation="sfs"):
     """Makes the msprime env compatible with gymnasium (see get_stdpopsim_model for getting demographic models)"""
     if task is None:
         task = MsprimeTask(target=None, observation=observation)
     
     if task.target is None:
-        task.target = simulate_target(model)
+        task.target = simulate_target(model, mutation_rate)
     
     return MSPRIMEEnv(model=model, task=task, tunable=tunable, randomize_start=randomize_start, max_steps=max_steps)
 
